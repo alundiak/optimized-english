55345c4667cedd502c1d4e6274cbabc87e660484
Andrii Lundiak <landike@gmail.com>
2015-06-10 00:46:32 +0200

    Changed code a bit. Simple code cleanup and refactoring.

--- javascripts/oe.js
+++ javascripts/oe.js
@@ -1,16 +1,22 @@
 //
 // Optimized English
 //
-function A(data){
+(function eventHandlers(data){
 
-    $('.inputText')
-    
+    $('.input-text')    
     .on("keyup", function(e){
-        $('.optimizedText').text( optimize(e.target.value) );
+        optimize(e.target.value);
     });
 
-}
-A({});
+    $('.optimize-btn').on('click', function(e){
+        e.preventDefault();
+        optimize( $('.input-text').val() );
+    });
+
+
+})();
+
+
 
 function optimize(text){
 
@@ -18,10 +24,28 @@ function optimize(text){
     // Here should go logic of Optimization
     //
 
-    return text;
+    var optimizedText = text;
+    var articlesPattern = /( a | an | the )+/
+    // TODO: Uppercase artciles as the start of sentence. 
+
+    try{
+        optimizedText = optimizedText.replace(articlesPattern, " ", "gi");
+    }catch(ex){}
+
+    // To Be Re-Thinked
+    // var doPattern = /( do | does )/ // But have to be used only between pronoun and verb. If after verb only, should still be.
+    // optimizedText = optimizedText.replace(doPattern, " ", "gi");
+
+    var verbEndingPattern = /(s|es)\b/ // Needto find and replace by nothing. But in case of VERBS only.
+    optimizedText = optimizedText.replace(verbEndingPattern, "", "gi");
+
+    $('.optimized-text').text(optimizedText);
+    $('.optimized-text-length').text(optimizedText.length);
+    $('.input-text-length').text( $('.input-text').val().length );
 
 }
 
+// TBD
 // $.ajax({
 //     url : "english.txt",
 //     dataType: "text",
