3e5457fcc19aaf1effc5dda71ebfc5c0677680a0
Andrii Lundiak <landike@gmail.com>
2017-05-04 01:02:13 +0200

    Ported files from gh-pages branch to master

---
+++ index.js
@@ -0,0 +1,52 @@
+//
+// Optimized English
+//
+(function eventHandlers(data) {
+    'use strict';
+
+    $('.input-text').on("keyup", function(e) {
+        optimize(e.target.value);
+    });
+
+    $('.optimize-btn').on('click', function(e) {
+        e.preventDefault();
+        optimize($('.input-text').val());
+    });
+
+})();
+
+function optimize(text) {
+
+    //
+    // Here should go logic of Optimization
+    //
+
+    var optimizedText = text;
+    var articlesPattern = /( a | an | the )+/;
+        // TODO: Uppercase articles as the start of sentence.
+
+    try {
+        optimizedText = optimizedText.replace(articlesPattern, " ", "gi");
+    } catch (ex) {}
+
+    // To Be Re-Thinked
+    // var doPattern = /( do | does )/ // But have to be used only between pronoun and verb. If after verb only, should still be.
+    // optimizedText = optimizedText.replace(doPattern, " ", "gi");
+
+    var verbEndingPattern = /(s|es)\b/; // Need to find and replace by nothing. But in case of VERBS only.
+    optimizedText = optimizedText.replace(verbEndingPattern, "", "gi");
+
+    $('.optimized-text').text(optimizedText);
+    $('.optimized-text-length').text(optimizedText.length);
+    $('.input-text-length').text($('.input-text').val().length);
+
+}
+
+// TBD
+// $.ajax({
+//     url : "english.txt",
+//     dataType: "text",
+//     success : function (data) {
+//         A(data);
+//     }
+// });
